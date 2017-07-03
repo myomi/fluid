60ad6608b588fbad31c1455a3f6c141a91cfeec4
myomi <myomit@gmail.com>
2017-07-03 12:26:19 +0900

    Modified directory structure.

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,6 +3,7 @@ import { app, BrowserWindow } from "electron";
 let win;
 
 function createWindow() {
+    console.log(`${__dirname}/index.html`);
     win = new BrowserWindow({ width: 800, height: 600 });
     win.loadURL(`file://${__dirname}/index.html`);
     win.on("closed", () => win = null);
