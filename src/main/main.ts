bb749a7427e4e38eb59d99ae447a280106354462
myomi <myomit@gmail.com>
2017-07-05 18:42:41 +0900

    Added Elm for renderer process.

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,9 +3,9 @@ import { app, BrowserWindow } from "electron";
 let win;
 
 function createWindow() {
-    console.log(`${__dirname}/index.html`);
     win = new BrowserWindow({ width: 800, height: 600 });
-    win.loadURL(`file://${__dirname}/index.html`);
+    // TODO fix url
+    win.loadURL(`file://${__dirname}/../../index.html`);
     win.on("closed", () => win = null);
 }
 
