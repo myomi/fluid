52ec20857feb25d2d3901969ed26049a57bef546
myomi <myomit@gmail.com>
2017-07-16 16:57:45 +0900

    Added port sample

--- src/renderer/Main.elm
+++ src/renderer/Main.elm
@@ -1,8 +1,46 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Html exposing (Html, text)
 
 
-main : Html msg
+port suggestions : (String -> msg) -> Sub msg
+
+
+type Msg
+    = Suggestions String
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "", Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    suggestions Suggestions
+
+
+view : Model -> Html Msg
+view model =
+    text model
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Suggestions label ->
+            ( label, Cmd.none )
+
+
+main : Program Never Model Msg
 main =
-    text "Hello, Elm & Electron"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,5 +1,8 @@
 import "./index.scss";
 import * as Elm from "./Main.elm";
 
-let container = document.getElementById('container');
-Elm.Main.embed(container);
+const container = document.getElementById('container');
+const app = Elm.Main.embed(container);
+
+app.ports.suggestions.send("John");
+
