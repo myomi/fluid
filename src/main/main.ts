61e115feaccc6ab02010cafe4e24f08eaffbf11e
myomi <myomit@gmail.com>
2017-07-16 15:29:14 +0900

    Added sqlite3

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,8 @@
 import { app, BrowserWindow } from "electron";
+import * as sqlite3 from "sqlite3";
+import * as fs from "fs";
+import * as path from "path";
+
 declare var __dirname: string
 
 let win : Electron.BrowserWindow;
@@ -8,6 +12,18 @@ function createWindow() {
     // TODO fix url
     win.loadURL(`file://${__dirname}/../renderer/index.html`);
     win.on("closed", () => win = null);
+
+    const base = path.join(home(), ".fluid");
+    if (!fs.existsSync(base)) {
+        fs.mkdirSync(base);
+    }
+    var dbFile = path.join(base, "db.sqlite3");
+    if (fs.existsSync(dbFile)) {
+        var db = new sqlite3.Database(dbFile);
+    } else {
+        var db = new sqlite3.Database(dbFile);
+        db.run('CREATE TABLE mytable (title STRING, id INTEGER, is_done BOOLEAN);');
+    }
 }
 
 app.on("ready", createWindow);
@@ -22,3 +38,7 @@ app.on("activate", () => {
     if (!win)
         createWindow();
 });
+
+function home(): string {
+    return process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+}
